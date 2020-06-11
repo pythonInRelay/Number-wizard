28942b70db9cf514fba159ae16f1461ac8a01cf0
pythoninrelay <S4F4gEEnzT#zxy6Eos>
2020-06-11 18:57:21 +0300

    Final commit!

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -6,39 +6,58 @@ public class NumberWizard : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    int minGuess = 1;
-    int maxGuess = 1000;
-    int guess = 500;
+    int minGuess;
+    int maxGuess;
+    int guess;
+    int numOfGuesses;
     
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        minGuess = 1;
+        maxGuess = 1000;
+        guess = 500;
+        numOfGuesses = 0;
+        
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log($"Please pick a number between {minGuess} and {maxGuess}.");
         Debug.Log("Now tell me if I need to guess higher or lower!");
         Debug.Log("'Up' = Higher, 'Down' = Lower and 'Enter' = Correct");
+        maxGuess += 1;
     }
-
+    
     // Update is called once per frame
     void Update()
     { 
         if (Input.GetKeyDown(KeyCode.UpArrow)) // If the key Up Arrow was pushed down, output a line to the console
         {
-            Debug.Log("You pressed the Up Arrow!");
-                minGuess = guess;
-                Debug.Log(guess);
+            minGuess = guess;
+            NextGuess();
+            
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("You pressed the Down Arrow!");
             maxGuess = guess;
-            Debug.Log(guess);
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("You pressed the Enter Key!");
-            Debug.Log("Yes that was my number!");
+            Debug.Log($"Great I guessed your number in only {numOfGuesses} guesses!");
+            Debug.Log("Let's play another round!");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+        guess = (maxGuess + minGuess) / 2;
+        numOfGuesses += 1;
+        Debug.Log($"Press the up arrow if your number is higher than {guess}!");
     }
 }
